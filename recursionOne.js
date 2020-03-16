a011e1c1deae4ebb8285ff33adedac912a0bffae
Matthew Wiltzius <matt.wiltzius@gmail.com>
2020-03-16 11:25:41 -0500

    inital commit

---
+++ recursionOne.js
@@ -0,0 +1,32 @@
+//https://medium.freecodecamp.org/learn-recursion-in-10-minutes-e3262ac08a1
+
+//Write a function that sums all numbers in an array that can have nested sub-arrays. Do not use loops.
+
+//simpler problem
+
+/*
+function sum(a, i){
+    if (i<0){
+        return 0;
+    } else {
+        return a[i] + sum(a, i - 1);
+    }
+}
+*/
+
+
+function sum(a, i){
+    if (i<0){
+        return 0;
+    }
+
+    let current = a[i]
+
+    if(Array.isArray(a[i])){
+        current = sum(a[i], a[i].length-1)
+    }
+
+    return current + sum(a, i - 1);
+}
+
+
---
+++ recursionTwo.js
@@ -0,0 +1,20 @@
+//https://medium.freecodecamp.org/learn-recursion-in-10-minutes-e3262ac08a1
+
+/*
+Write a function that generates all possible combinations of 1 and 0 for n bits. 
+For example, if the function receives 2 as the number of bits, it should produce the following 4 combinations: 00,01,10,11. 
+You cannot use any mathematical operators.
+*/
+
+function binary(n) {
+    if ( n===1 ) {
+        console.log(a.join(''));
+    } else {
+        a[n-1] = 0;
+        binary(i-1);
+        a[n-1] = 1;
+        binary(i-1);
+    }
+}
+
+//gross
